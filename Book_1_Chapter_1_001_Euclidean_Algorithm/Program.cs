1467fabf9fb2fcd6dfcce9983ff86deca2fd374e
Ekaterina <tiurina.kate@gmail.com>
2022-06-13 10:17:17 +0300

    Book1 Chapter 1 Euclidean Algorithm

---
+++ Book_1_Chapter_1_001_Euclidean_Algorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿// Даны два целых положительных числа m и n.
+//Найти НОД.
+Console.WriteLine("Input the first number:");
+int m = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"m={m}");
+Console.WriteLine("Input the second number:");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"n={n}");
+while(m!=n)
+{
+    if(m>n)
+    {
+        m=m-n;
+    }
+    else
+    {
+        n=n-m;
+    }
+}
+int nod = n;
+Console.WriteLine($"Greatest common divisor = {nod}");
